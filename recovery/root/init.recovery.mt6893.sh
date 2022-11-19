fac0864849064a1bdc61e4885a31144fcf23a3da
Nishant Kumar <www.rajsonu13@gmail.com>
2022-11-19 13:09:08 +0530

    denniz: Keep margin in height and width
    * Also set animation fps to 60
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Change-Id: Ie0af31e1f9e3a0080f4687664fee6dca201d43d2

--- recovery/root/init.recovery.mt6893.sh
+++ recovery/root/init.recovery.mt6893.sh
@@ -1,3 +1,6 @@
 #!/system/bin/sh
 
 echo 1022 > /sys/class/leds/lcd-backlight/brightness
+setprop ro.recovery.ui.margin_height 150
+setprop ro.recovery.ui.animation_fps 60
+setprop ro.recovery.ui.margin_width 50
