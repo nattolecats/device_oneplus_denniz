ddd3632718570508e95468be8dea1cda3305ec66
lahaina <nattolecats@gmail.com>
2023-05-25 18:21:09 +0900

    denniz: vendorsetup: Refactor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,49 +1,41 @@
 #!/bin/sh
 
-# Guard
-if ! history | tail -n 1 | grep -q "evolution_denniz"; then 
+# Memory the current directory
+CUR_DIR=$(pwd)
 
-	croot
-	cd build/soong  && git reset --hard
-	
-	croot
-	cd frameworks/av && git reset --hard
-	
-	croot
-	cd frameworks/base && git reset --hard
-	
-	croot
-	cd frameworks/native && git reset --hard
-	
-	croot
-	cd packages/modules/Bluetooth && git reset --hard
-	
-	croot
-	return
-	
-fi
+croot && export ANDROID_BUILD_TOP=$(pwd)
+
+# Download Source Patches
+PATCH=vendor/oneplus/denniz-patches/functions.sh
+if ! [ -a $PATCH ]; then git clone https://github.com/nattolecats/vendor_oneplus_denniz-patches vendor/oneplus/denniz-patches ; fi
+
+# Include common patching functions
+source vendor/oneplus/denniz-patches/functions.sh
+
+# Discard existing patches
+discardPatches
+
+# Guard
+if ! history | tail -n 1 | grep -q "evolution_denniz"; then return; fi
 
 # Give it officiality
 export EVO_BUILD_TYPE=OFFICIAL
 
 # Go to root of source
-croot
+cd "$ANDROID_BUILD_TOP"
 
 # Prebuilt apps
 PA=packages/apps/prebuilt-apps/prebuilt-apps.mk
-if ! [ -a $PA ]; then git clone https://github.com/nattolecats/packages_apps_prebuilt-apps packages/apps/prebuilt-apps/ ; fi
+if ! [ -a $PA ]; then git clone https://github.com/nattolecats/packages_apps_prebuilt-apps packages/apps/prebuilt-apps ; fi
 
 # Firmware images
 FW=vendor/firmware/denniz/firmware.mk
 if ! [ -a $FW ]; then git clone https://github.com/nattolecats/vendor_firmware_denniz vendor/firmware/denniz ; fi
 
-# Source Patches
-PATCH=vendor/oneplus/denniz-patches/applyPatchesV2.sh
-if ! [ -a $PATCH ]; then git clone https://github.com/oneplus-mt6893-devs/prebuilt_vendor_oneplus_denniz-patches vendor/oneplus/denniz-patches ; fi
-
 # Apply it.
-cd vendor/oneplus/denniz-patches/ ; bash applyPatchesV2.sh
+applyPatches
 
-cd - > /dev/null
+# Return cd to memoried directory
+cd $CUR_DIR ; unset CUR_DIR
 
 return
