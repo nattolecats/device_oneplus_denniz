e2f08d55535de96a36787d48d7d0c8fbeef43fad
lahaina <nattolecats@gmail.com>
2023-06-15 01:28:43 +0900

    denniz: Copy task_profiles and cgroups from source * This improve system performance.

--- device.mk
+++ device.mk
@@ -165,8 +165,8 @@ PRODUCT_COPY_FILES += \
 
 # Perf
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
-    $(DEVICE_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Recovery
 PRODUCT_PACKAGES += \
