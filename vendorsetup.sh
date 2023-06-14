3b03bb693f2bddde47500ce4ca4610d53ce42a33
Christopher N. Hesse <raymanfx@gmail.com>
2023-06-14 20:28:22 +0900

    denniz: Hexedit libmtk-ril to fix incoming calls Taken from: https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,6 +32,16 @@ if ! [ -a $PA ]; then git clone https://github.com/nattolecats/packages_apps_pre
 FW=vendor/firmware/denniz/firmware.mk
 if ! [ -a $FW ]; then git clone https://github.com/nattolecats/vendor_firmware_denniz vendor/firmware/denniz ; fi
 
+# Hack MTK RIL libs for USSD and incoming calls
+# see https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086
+BLOB_ROOT="$ANDROID_BUILD_TOP"/vendor/oneplus/denniz/proprietary
+
+for blob in $BLOB_ROOT/lib64/libmtk-ril.so; do
+    sed -i \
+        -e 's/AT+EAIC=2/AT+EAIC=3/g' \
+        $blob
+done
+
 # Apply it.
 applyPatches
 
