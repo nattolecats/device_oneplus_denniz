f1c892dc19cdb54840c263b7ba853f42bd36fec4
lahaina <nattolecats@gmail.com>
2023-10-31 19:48:18 +0900

    denniz: Import GCamGo

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nexus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/nexus.xml
 
+# GCamGo
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V3_8
+
 # For debugging
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += MatLog
